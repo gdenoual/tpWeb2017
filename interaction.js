6b162ac1f8298f91efb995261cf5bcbe6cb89b90
Gwendal Denoual <12006684@e103m03.istic.univ-rennes1.fr>
2017-01-31 12:04:03 +0100

    Creation methode d'ecoute canvas , creation modele

--- interaction.js
+++ interaction.js
@@ -3,10 +3,62 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+	
+	var x0 = 0;
+	var y0 = 0;
+	var x1 = 0;
+	var y1 = 0;
+	var press = false;
 
 	// Developper les 3 fonctions gérant les événements
 
+
+	  this.pression = function(evt) {
+		console.log(evt);
+		var res = getMousePosition(canvas, evt) ;
+
+		this.x0 = res.x;
+		this.y0 = res.y;		
+
+		console.log("X0  : " + this.x0 +" Y0 : " +  this.y0);
+   		press= true;
+ 		 }.bind(this);
+	// Associer la fonction a la classe , le protype ( pour alan) =) 
+
+
+	  this.deplacement = function(evt) {
+
+		if(press){
+		var res = getMousePosition(canvas, evt) ;
+
+		console.log("X0  : " + this.x0 +" Y0 : " +  this.y0);
+		this.x1 = res.x;
+		this.y1 = res.y;
+		console.log("X1  : " + this.x1 +" Y1 : " +  this.y1);
+   		 }
+		
+		}.bind(this);	
+
+
+	  this.relachement = function(evt) {
+
+		var res = getMousePosition(canvas, evt) ;
+
+		this.x1 = res.x;
+		this.y1 = res.y;
+		console.log("X1  : " + this.x1 +" Y1 : " +  this.y1);
+
+
+   		 press= false;
+ 		 }.bind(this);
+
+
+
 	// Associer les fonctions précédentes aux évènements du canvas.
+
+		canvas.addEventListener('mousedown', this.pression, false);
+		canvas.addEventListener('mousemove', this.deplacement, false);
+		canvas.addEventListener('mouseup', this.relachement, false);
 };
 
 
--- model.js
+++ model.js
@@ -1,3 +1,33 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function drawing(canvas, evt){
+
+	var maListe = new Array();
+}
+
+
+function forme(canvas, evt){
+	var couleur; 
+	var epaisseur; 
+
+}
+
+
+function rectangle(canvas , evt){
+
+	forme.call(this, canvas, evt);
+	var hauteur;
+	var largeur;
+
+}
+
+function ligne(canvas, evt) {
+
+
+	forme.call(this,canvas , evt);
+	var coordDebut;
+	var coordFin;
+
+} 
